2fdcf87dc36d2d30a6f6598302d862f0c262dffb
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2022-09-22 11:00:44 +0300

    vue compositon api shopping app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import {ref} from "vue"
+const header = ref("Shopping List App")
+const items = ref([
+])
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const newItem = ref("")
+const editing = ref(false)
+const newItemHighPriority = ref(false)
+const saveItem = () => {
+  items.value.push({id: items.value.length+1,label: newItem.value})
+  newItem.value = ""
 }
+const doEdit = (e) => {
+    editing.value = e
+    newItem.value = ""
+}
+
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+<template>
+<div class="header">
+  <h1>{{header}}</h1>
+  <button class="btn" v-if="editing" @click="doEdit(false)">Cancel</button>
+  <button v-else class="btn btn-primary" @click="doEdit(true)">Add Item</button>
+</div>
+
+  <div class="add-item-form" v-if="editing" >
+  <input type="text" placeholder="Add an item" v-model.trim="newItem">
+  <label>
+    <input type="checkbox" v-model="newItemHighPriority">
+    High Priority
+  </label>
+  <br>
+
+  <button class="btn btn-primary" @click="saveItem">
+    Save Item
+  </button>
+  </div>
+
+  <ul v-if="editing">
+    <li  v-for="({id,label}) in items" :key="id">{{label}}</li>
+  </ul>
+  <p v-if="!items.length" >
+    Nothing to see here
+  </p>
+
+</template>
