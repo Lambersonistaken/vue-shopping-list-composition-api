3f70d4beff13e4884e1cac3b8bd15fff618128d7
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2022-09-22 13:09:07 +0300

    vue compositon api shopping app finished :zap:

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <script setup>
-import {ref} from "vue"
+import {ref,computed} from "vue"
 const header = ref("Shopping List App")
 const items = ref([
+  {id:1,label:"10 party hats",purchased: true,highPriority:false},
+  {id:1,label:"2 board games",purchased: true,highPriority:false},
+  {id:1,label:"20 cups",purchased: false,highPriority:true}
 ])
 
 const newItem = ref("")
 const editing = ref(false)
+const characterCount = computed(() => {
+  return newItem.value.length
+})
 const newItemHighPriority = ref(false)
 const saveItem = () => {
-  items.value.push({id: items.value.length+1,label: newItem.value})
+  items.value.push({id: items.value.length+1,label: newItem.value,highPriority: newItemHighPriority.value})
   newItem.value = ""
+  newItemHighPriority.value = ""
 }
 const doEdit = (e) => {
     editing.value = e
     newItem.value = ""
 }
-
+const togglePurchased= (item) => {
+  item.purchased = !item.purchased
+}
+const reversedItems = computed(() => {
+  return [...items.value].reverse()
+})
 
 </script>
 
@@ -35,13 +47,16 @@ const doEdit = (e) => {
   </label>
   <br>
 
-  <button class="btn btn-primary" @click="saveItem">
+  <button class="btn btn-primary" @click="saveItem" v-bind:disabled="newItem.length === 0">
     Save Item
   </button>
   </div>
+  <p class="counter" v-if="editing">
+    {{characterCount}}/200
+  </p>
 
-  <ul v-if="editing">
-    <li  v-for="({id,label}) in items" :key="id">{{label}}</li>
+  <ul >
+    <li @click="togglePurchased(item)"  v-for="(item) in reversedItems" :key="item.id"  class="static-class" :class="{strikeout: item.purchased, priority:item.highPriority}">{{item.label}}</li>
   </ul>
   <p v-if="!items.length" >
     Nothing to see here
